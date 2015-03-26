d45dbb572e87ef321a5761d02009641f70f2bc5e
James Patton <jamesyp@gmail.com>
2015-03-26 14:24:42 -0700

    Chapter 3 arrayification

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 // welcome the user
 alert("Welcome to Quiz Ninja!");
 
-var question = "What is Superman's real name?";
-var answer = prompt(question);
-alert("You answered " + answer);
+var quiz = [
+	["What is Superman's real name?", "Kal El"],
+	["What is Wonderwoman's real name?", "Dianna"],
+	["What is Batman's real name?", "Wayne Bruce"],
+];
+
+var score = 0;
+
+for (var i = 0; i < quiz.length; i++) {
+	var answer = prompt(quiz[i][0]);
+
+	if (answer === quiz[i][1]) {
+		alert("Correct!");
+		score++;
+	} else {
+		alert("WRONGO BOZO");
+	}
+}
+
+alert("Game over, you scored " + score + " points");
